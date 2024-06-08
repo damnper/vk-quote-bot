482f2be22978c481116f979d08c96e2ed11d101b
Daler Yunusov <daleryunusov2001@gmail.com>
2024-06-08 08:28:32 +0300

    Initial commit

---
+++ src/main/java/com/vkbot/vkquotebot/VkQuoteBotApplication.java
@@ -0,0 +1,13 @@
+package com.vkbot.vkquotebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkQuoteBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VkQuoteBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vkbot/vkquotebot/VkQuoteBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vkbot.vkquotebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkQuoteBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
